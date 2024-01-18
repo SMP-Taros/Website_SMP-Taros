1f0105f62cea88adf2929655f3d780b0930e4327
Muhammad <muhammadahjahh@gmail.com>
2024-01-18 20:24:49 +0800

    tolong baikin img nya zam :v

--- profile-page/src/components/FullWeb.jsx
+++ profile-page/src/components/FullWeb.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import WebKompetensi from './WebKompetensi';
 import WebVisi from './WebVisi';
+import WebSambutan from "./WebSambutan";
 
 function FullWeb() {
     return (
         <div className="profile-page">
+            <WebSambutan></WebSambutan>
             <WebVisi></WebVisi>
             <WebKompetensi></WebKompetensi>
         </div>
---
+++ profile-page/src/components/WebSambutan.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function WebSambutan() {
+    return (
+        <div className="sambutan-container">
+            <div className="sambutan-text">
+                <h1>SAMBUTAN KEPALA SEKOLAH</h1>
+                <p>Assalamualaikum Warahmatullahi Wabarakatuh</p>
+            </div>
+            <img src="metz.jpg" />
+        </div>
+    )
+}
+
+export default WebSambutan;

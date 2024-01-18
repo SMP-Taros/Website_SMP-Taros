b7d76e277e9249cc7ea8331a61b065ea3633000f
Muhammad <muhammadahjahh@gmail.com>
2024-01-18 17:48:32 +0800

    komponen 1

---
+++ profile-page/src/components/FullWeb.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WebKompetensi from './WebKompetensi';
+
+function FullWeb() {
+    return (
+        <div className="profile-page">
+            <WebKompetensi></WebKompetensi>
+        </div>
+    )
+}
+
+export default FullWeb;
---
+++ profile-page/src/components/WebVisi.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WebVisi() {
+    return (
+        <div className='visi-container'>
+            <h1 className='visi-header'></h1>
+            <p></p>
+        </div>
+    )
+}
+
+export default WebVisi;

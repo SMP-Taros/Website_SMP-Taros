f2fd162064d3c9b1754cc743331427c4688f0c56
Muhammad <muhammadahjahh@gmail.com>
2024-01-18 18:34:03 +0800

    komponen 2 - visi

--- profile-page/src/components/FullWeb.jsx
+++ profile-page/src/components/FullWeb.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import WebKompetensi from './WebKompetensi';
+import WebVisi from './WebVisi';
 
 function FullWeb() {
     return (
         <div className="profile-page">
+            <WebVisi></WebVisi>
             <WebKompetensi></WebKompetensi>
         </div>
     )

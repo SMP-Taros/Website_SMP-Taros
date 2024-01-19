19d8a0f61eac7e03d897f67bc969ef0ce8057075
Muhammad <muhammadahjahh@gmail.com>
2024-01-19 19:57:17 +0800

    komponen banner

--- profile-page/src/components/FullWeb.jsx
+++ profile-page/src/components/FullWeb.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import WebKompetensi from './WebKompetensi';
 import WebVisi from './WebVisi';
 import WebSambutan from "./WebSambutan";
+import WebBanner from "./WebBanner";
 
 function FullWeb() {
     return (
         <div className="profile-page">
+            <WebBanner></WebBanner>
             <WebSambutan></WebSambutan>
             <WebVisi></WebVisi>
             <WebKompetensi></WebKompetensi>

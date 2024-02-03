6d66cb8a47e192e80e02491878f82ebe382472fb
Muhammad <muhammadahjahh@gmail.com>
2024-02-04 00:29:16 +0800

    apdet

--- profile-page/src/components/WebDetailBerita.jsx
+++ profile-page/src/components/WebDetailBerita.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 import Jam from '../image/clock.svg';
 import { useParams } from 'react-router-dom';
-import { listBerita } from '../data/data-berita'; 
+import { listBerita } from '../data/data-berita';
+import gambar from '../image/contoh.jpg'
 
 function WebDetailBerita() {
     const { id } = useParams();
     const postList = listBerita();
-    const post = postList.find((post) => post.id == id )
+    const post = postList.find((post) => post.id == id)
 
     if (!post) {
         return <p>Berita tidak ditemukan</p>;
-      }
+    }
 
     return (
         <div className='detail-berita-container'>
             <div className='detail-berita-isi'>
-                <img src={post.image} className='detail-gambar' />
-                <div className='tanggal'>
-                    <img src={Jam} className='icon-tanggal' />
-                    <div className='text-tanggal'>{post.date}</div>
+                <div className='row-kiri'>
+                    <div className='judul'>{post.title}</div>
+                    <div className='tanggal'>
+                        <img src={Jam} className='icon-tanggal' />
+                        <div className='text-tanggal'>{post.date}</div>
+                    </div>
+                    <img src={post.image} className='detail-gambar' />
+                    <div className='text-berita'>{post.text}</div>
                 </div>
-                <div className='judul'>{post.title}</div>
-                <div className='text-berita'>{post.text}</div>
+                <div className='row-kanan'>
+
+                </div>
             </div>
         </div>
     )

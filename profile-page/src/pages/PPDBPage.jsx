2191181440aab23c3b9e36c50401a8a8fc80bc91
Muhammad <muhammadahjahh@gmail.com>
2024-02-05 19:11:13 +0800

    konponen WebBannerButton

---
+++ profile-page/src/pages/PPDBPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import WebHeader from '../components/WebHeader'
+import WebNavbar from '../components/WebNavbar'
+import WebFooter from '../components/WebFooter'
+import WebBannerButton from '../components/WebBannerButton'
+
+function PPDBPage() {
+    return (
+        <div>
+            <WebHeader />
+            <WebNavbar />
+            <WebBannerButton />
+            <WebFooter />
+        </div>
+    )
+}
+
+export default PPDBPage;
